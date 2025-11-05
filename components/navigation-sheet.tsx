404aebc1124e1c4799d55fd565be724b29616c0e
AbdulganiK <abdulganikorkmaz589@gmail.com>
2025-11-05 11:06:43 +0100

    first commit

---
+++ components/navigation-sheet.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import { Logo } from "@/components/logo";
+
+export const NavigationSheet = () => {
+  return (
+    <Sheet>
+      <VisuallyHidden>
+        <SheetTitle>Navigation Menu</SheetTitle>
+      </VisuallyHidden>
+
+      <SheetTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="px-6 py-3">
+        <Logo />
+
+        <div className="mt-12 text-base space-y-4">
+          <Link href="#" className="inline-block">
+            Home
+          </Link>
+
+        
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
